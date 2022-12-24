9877253234003ce799fc905487aeb43c5045487b
Phil Cruz <mfel.cruz@gmail.com>
2022-12-24 17:07:04 -0500

    meta description improved

--- pages/_document.js
+++ pages/_document.js
@@ -4,13 +4,13 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="GPT-3 Writer" key="title"/>
-        <meta property="og:description" content="build with buildspace" key="description"/>
+        <meta property="og:title" content="Magic Lyrics Generator" key="title"/>
+        <meta property="og:description" content="Magic Lyrics Generator" key="description"/>
         <meta
           property="og:image"
-          content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
+          content="/ScreenShot.png"
         />
-        <meta name="twitter:card" content="summary_large_image"></meta>
+        <meta name="twitter:card" content="/ScreenShot.png"></meta>
       </Head>
       <body>
         <Main />
